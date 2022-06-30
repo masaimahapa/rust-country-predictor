8ccd9f88ec29b375d5abe21fc811be3d0637e4f0
masai <mahapamasai@gmail.com>
2022-06-30 17:43:29 +0200

    extracted show predictions from turn_into_json

--- src/lib.rs
+++ src/lib.rs
@@ -28,18 +28,20 @@ pub async fn fetch_data(name : &str) -> Result<String, Box<dyn error::Error>>{
 use serde::Deserialize;
 use serde_json;
 
-pub fn typed_example(text : &str) -> serde_json::Result<()> {
-    let p: NamePredictions = serde_json::from_str(text)?;
-    let possible_countries = p.country;
+pub fn turn_into_json(text : &str) -> serde_json::Result<NamePredictions> {
+    let name_predictions: NamePredictions = serde_json::from_str(text)?;
+    Ok(name_predictions)
+}
+
+pub fn show_predictions(preds : NamePredictions){
+    let possible_countries = preds.country;
 
     if possible_countries.len() == 0 {
-        println!("Hi {:?}. I can't seem to figure out which country you're from.", p.name);
+        println!("Hi {:?}. I can't seem to figure out which country you're from.", preds.name);
     } else {
-        println!("Hi {:?}. I think you are from one of the following countries:", p.name);
+        println!("Hi {:?}. I think you are from one of the following countries:", preds.name);
         for (index, c) in possible_countries.iter().enumerate() {
             println!("{} . {} ", index+1, c.country_id);
         }
     }
-    
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ if args.len() >1 {
     process::exit(1);
 }
 
+let result = cli::fetch_data(name);
 
-let res = cli::fetch_data(name);
+let obj_json = cli::turn_into_json(&result.await.expect("could not load"));
 
-let obj = cli::typed_example(&res.await.expect("could not load"));
-
-println!("{:?}", obj);
+let predictions = obj_json.unwrap();
+cli::show_predictions(predictions);
 }
